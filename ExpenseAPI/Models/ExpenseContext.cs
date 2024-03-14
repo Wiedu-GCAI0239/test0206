cd7f7955a9fed78ac1d4f5b0cc0d7b395c043c0d
Wiedu-GCAI0239 <157328976+Wiedu-GCAI0239@users.noreply.github.com>
2024-03-14 03:49:22 +0000

    full-backup

---
+++ ExpenseAPI/Models/ExpenseContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseAPI.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
+
